26acf9a571c0da16a56830b2d9de7fd7755d1d20
ismail-ru-cse-39 <shaikat.ru.39@gmail.com>
2020-09-10 17:31:59 +0000

    header and style file is created and included in index file.

---
+++ admin/index.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin | Home</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<?php include("inc/header.php"); ?>
+</body>
+</html>
